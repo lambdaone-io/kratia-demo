f138e6c9cb100bbd01b2d3a51a562ba54fbae66f
Francisco Aramburo <francoara@pm.me>
2018-12-03 23:38:07 +0100

    cleanup

--- src/Api.elm
+++ src/Api.elm
@@ -1,51 +1,63 @@
 module Api exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (class, href, value, src, width)
-import Html.Events exposing (onClick)
-import Http exposing (..)
-import List exposing (filter, head)
-import Json.Decode as Decode
-import Json.Decode.Pipeline exposing (required, optional)
-import Shared exposing (Msg(..))
+{-| This module is responsible for communicating to the Kratia API. -}
+
+
+import Url.Builder exposing (QueryParameter)
+import Http exposing (Body, Expect)
 import Json.Decode as Decode exposing (Decoder, Value, field, string)
 import Json.Encode as Encode
 
 
-type alias RegistrationRequest =
-    { community: String
-    , data: String
-    }
-
-type alias RegistrationResponse =
-    {  member: String
-    }
+-- CRED
 
 
--- DECODERS
+type alias Token = String
 
-registrationResponseDecoder : Decode.Decoder RegistrationResponse
-registrationResponseDecoder =
-    Decode.succeed RegistrationResponse
-        |> required "member" Decode.string
+{-| The authentication credentials for the Viewer (that is, the currently logged-in user.)
+This includes:
+  - The cred's Username
+  - The cred's authentication token
+By design, there is no way to access the token directly as a String.
+It can be encoded for persistence, and it can be added to a header
+to a HttpBuilder for a request, but that's it.
+This token should never be rendered to the end user, and with this API, it
+can't be!
+-}
+type Cred = Cred String Token
 
--- Operations
+
+username : Cred -> String 
+username (Cred name _) = name
+
+
+credHeader : Cred -> Http.Header
+credHeader (Cred _ token) = 
+    Http.header "Authorization" ("Bearer " ++ token)
+
+
+
+-- Endpoints
 
 
 rootCommunity : String
 rootCommunity = "19ce7b9b-a4da-4f9c-9838-c04fcb0ce9db"
 
 
-register : String -> String  -> Cmd Msg
-register server nickname =
- Http.post (server ++ "/api/v1/registry")
-   (Http.jsonBody <| Encode.object
-     [ ( "community", Encode.string rootCommunity )
-     , ( "data", Encode.string nickname )
-     ]) registrationResponseDecoder
-     |> Http.send (\resp -> Registered (case resp of
-                                         Err e -> Err e
-                                         Ok r -> Ok r.member
-                                         ) nickname)
+url : String -> List String -> List QueryParameter -> String
+url hostname paths queryParams =
+  Url.Builder.crossOrigin hostname
+      ("api" :: "v1" :: paths)
+      queryParams
 
--- Help, this ^^^ should be a simple Result.map
+
+register : String -> String -> (Result Http.Error Cred -> msg) -> Cmd msg
+register hostname nickname msg =
+    Http.post 
+      { url = url hostname ["registry"] []
+      , body = Http.jsonBody <| Encode.object 
+        [ ( "community", Encode.string rootCommunity )
+        , ( "data", Encode.string nickname )
+        ]
+      , expect = Http.expectJson (\result -> msg <| Result.map (Cred nickname) result ) (field "member" string)
+      }
