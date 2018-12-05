16d58e486834d32cb15f025bc5d9f0bd363191c5
Francisco Aramburo <francoara@pm.me>
2018-12-05 02:09:43 +0100

    modularisation

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,21 @@
 module Main exposing (main)
 
-import Api exposing (Cred, register, username)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onSubmit, onInput)
 
-import Http exposing (Body, Expect)
-import Json.Decode as Decode exposing (Decoder, Value, field, string)
-import Json.Encode as Encode
-
-import Browser.Navigation as Navigation
+import Browser.Navigation as Nav
 import Browser exposing (UrlRequest)
-import Page.Registration exposing (userView)
 import Url exposing (Url)
 import Url.Parser as UrlParser exposing ((</>), Parser, s, top)
+import Html exposing (Html, div, text, h1)
+import Html.Attributes exposing (..)
+
 import Bootstrap.Navbar as Navbar
 import Bootstrap.Grid as Grid
-import Bootstrap.Form as Form
 import Bootstrap.Grid.Col as Col
-import Bootstrap.Card as Card
-import Bootstrap.Card.Block as Block
-import Bootstrap.Button as Button
-import Bootstrap.ListGroup as Listgroup
-import Bootstrap.Modal as Modal
-import Bootstrap.Form.Input as Input
-import Bootstrap.Button as Button
 
-import Shared exposing (..)
+import Api exposing (Cred, username, Service(..))
+import Config exposing (Config, Flags, fromFlags)
+import Route exposing (Route)
+import Page.Registration as Reg
 
 
 
@@ -37,30 +26,53 @@ main =
         , view = view
         , update = update
         , subscriptions = subscriptions
-        , onUrlRequest = ClickedLink
-        , onUrlChange = UrlChange
+        , onUrlRequest = LinkClicked
+        , onUrlChange = UrlChanged
         }
 
 
-init : Flags -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+
+-- MODEL
+
+
+type alias Model =
+    { credentials : Maybe Cred
+    , config : Config
+    , navKey : Nav.Key
+    , navState : Navbar.State
+    , page : Page
+    }
+
+
+type Page
+    = Init
+    | About
+    | Registration Reg.Model
+    | Ballots
+    | NotFound
+
+init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    let
+    let 
+        config = fromFlags flags
+
         ( navState, navCmd ) =
             Navbar.initialState NavMsg
-
-        ( model, urlCmd ) =
-            urlUpdate url
-            { maybeCred = Nothing
-            , navKey = key
-            , navState = navState
-            , page = Home
-            , modalVisibility = Modal.hidden
-            , nicknameInput = ""
-            , loading = False
-            , flags = flags
-            }
+        
+        ( model, routeCmd ) =
+            changeRouteTo (Route.fromUrl url)
+                { credentials = Nothing
+                , config = config
+                , navKey = key
+                , navState = navState
+                , page = Init
+                }
     in
-        ( model, Cmd.batch [ urlCmd, navCmd] )
+        ( model, Cmd.batch [ routeCmd, navCmd ] )
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
@@ -68,62 +80,68 @@ subscriptions model =
     Navbar.subscriptions model.navState NavMsg
 
 
+
+-- UPDATE 
+
+
+type Msg
+    = RouteChanged (Maybe Route)
+    | UrlChanged Url
+    | LinkClicked UrlRequest
+    | NavMsg Navbar.State
+    | RegMsg Reg.Msg
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
+    case ( msg, model.page ) of
+        ( RouteChanged route, _ ) ->
+            changeRouteTo route model
 
-        ClickedLink req ->
+        ( UrlChanged url, _ ) -> 
+            changeRouteTo (Route.fromUrl url) model
+
+        ( LinkClicked req, _ ) ->
              case req of
-                 Browser.Internal url -> ( model, Navigation.pushUrl model.navKey <| Url.toString url )
-                 Browser.External href -> ( model, Navigation.load href )
+                 Browser.Internal url -> ( model, Nav.pushUrl model.navKey <| Url.toString url )
+                 Browser.External href -> ( model, Nav.load href )
 
-        UrlChange url -> 
-            urlUpdate url model
-
-        NavMsg state -> 
+        ( NavMsg state, _ ) -> 
             ( { model | navState = state } , Cmd.none )
 
-        CloseModal -> 
-            ( { model | modalVisibility = Modal.hidden } , Cmd.none )
+        ( RegMsg subMsg, Registration registration ) -> 
+            Reg.update subMsg registration
+                |> updateWith Registration RegMsg model
 
-        ShowModal -> 
-            ( { model | modalVisibility = Modal.shown } , Cmd.none )
-
-        Registered (Ok cred) -> 
-            ( { model | maybeCred = Just cred } , Cmd.none )
-
-        Registered (Err err) -> 
+        ( _, _ ) ->
+            -- Disregard messages that arrived for the wrong page.
             ( model, Cmd.none )
 
-        EnteredNickname nickname -> 
-            ( { model | nicknameInput = nickname }, Cmd.none )
 
-        RegisterMember -> 
-            ( { model | loading = True }, register model.flags.services.kratia model.nicknameInput Registered)
-
-
-urlUpdate : Url -> Model -> ( Model, Cmd Msg )
-urlUpdate url model =
-    case decode url of
+changeRouteTo : Maybe Route -> Model -> ( Model, Cmd Msg )
+changeRouteTo maybeRoute model =
+    case maybeRoute of
         Nothing ->
             ( { model | page = NotFound }, Cmd.none )
+        
+        Just Route.About ->
+            ( { model | page = About }, Cmd.none )
 
-        Just route ->
-            ( { model | page = route }, Cmd.none )
+        Just Route.Registration ->
+            Reg.init model.config.kratia model.credentials
+                |> updateWith Registration RegMsg model
+
+        Just Route.Ballots ->
+            ( { model | page = Ballots }, Cmd.none )
 
 
-decode : Url -> Maybe Page
-decode url =
-    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
-    |> UrlParser.parse routeParser
+updateWith : (subModel -> Page) -> (subMsg -> Msg) -> Model -> ( subModel, Cmd subMsg ) -> ( Model, Cmd Msg )
+updateWith toModel toMsg model ( subModel, subCmd ) =
+    ( { model | page = toModel subModel }, Cmd.map toMsg subCmd )
 
 
-routeParser : Parser (Page -> a) a
-routeParser =
-    UrlParser.oneOf
-        [ UrlParser.map Home top
-        , UrlParser.map GettingStarted (s "some-page")
-        ]
+
+-- VIEW
 
 
 view : Model -> Browser.Document Msg
@@ -133,7 +151,6 @@ view model =
         [ div []
             [ menu model
             , mainContent model
-            , modal model
             ]
         ]
     }
@@ -146,13 +163,12 @@ menu model =
         |> Navbar.container
         |> Navbar.brand [ href "#" ] [ text "Kratia" ]
         |> Navbar.items
-            [ Navbar.itemLink [ href "#some-page" ] [ text "Some page" ]
-            ]
+            [ Navbar.itemLink [ href "#about" ] [ text "About" ] ]
         |> Navbar.customItems
             [ Navbar.textItem []
             [ let
                content =
-                case model.maybeCred of
+                case model.credentials of
                   Nothing -> "Please, login"
                   Just cred ->  "Welcome, " ++ (username cred)
               in text content] ]
@@ -163,44 +179,33 @@ mainContent : Model -> Html Msg
 mainContent model =
     Grid.container [] <|
         case model.page of
-            Home ->
-                pageHome model
-
-            GettingStarted ->
-                pageGettingStarted model
-
+                
             NotFound ->
                 pageNotFound
 
+            Registration registration ->
+                [ Reg.view registration ]
 
+            Ballots ->
+                [ h1 [] [ text "Ballots" ] ]
+
+            About ->
+                [ h1 [] [ text "About" ] ]
+
+
+{-
 pageHome : Model -> List (Html Msg)
 pageHome model =
-    [
-     Grid.row []
+    [ Grid.row []
         [ Grid.col []
             [ Card.config [ Card.outlinePrimary ]
                 |> Card.headerH4 [] [ text "Kratia Demo" ]
-                |> Card.block []
-                    [  Block.custom <| userView model
-                    ]
+                |> Card.block [] [ Block.custom <| Reg.view model ]
                 |> Card.view
             ]
-
         ]
     ]
-
-
-pageGettingStarted : Model -> List (Html Msg)
-pageGettingStarted model =
-    [ h2 [] [ text "Getting started" ]
-    , Button.button
-        [ Button.success
-        , Button.large
-        , Button.block
-        , Button.attrs [ onClick ShowModal ]
-        ]
-        [ text "Click me" ]
-    ]
+-}
 
 
 pageNotFound : List (Html Msg)
@@ -208,23 +213,3 @@ pageNotFound =
     [ h1 [] [ text "Not found" ]
     , text "Sorry couldn't find that page"
     ]
-
-
-modal : Model -> Html Msg
-modal model =
-    Modal.config CloseModal
-        |> Modal.small
-        |> Modal.h4 [] [ text "Getting started ?" ]
-        |> Modal.body []
-            [ Grid.containerFluid []
-                [ Grid.row []
-                    [ Grid.col
-                        [ Col.xs6 ]
-                        [ text "Col 1" ]
-                    , Grid.col
-                        [ Col.xs6 ]
-                        [ text "Col 2" ]
-                    ]
-                ]
-            ]
-        |> Modal.view model.modalVisibility
--- src/Page/Registration.elm
+++ src/Page/Registration.elm
@@ -1,41 +1,89 @@
-module Page.Registration exposing (userView)
-
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onSubmit, onInput)
+module Page.Registration exposing (Model, init, Msg, update, view)
 
 import Browser.Navigation as Navigation
 import Browser exposing (UrlRequest)
-import Url exposing (Url)
-import Url.Parser as UrlParser exposing ((</>), Parser, s, top)
-import Bootstrap.Navbar as Navbar
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onSubmit, onInput)
+import Http as Http
+
 import Bootstrap.Grid as Grid
 import Bootstrap.Form as Form
 import Bootstrap.Grid.Col as Col
-import Bootstrap.Card as Card
-import Bootstrap.Card.Block as Block
 import Bootstrap.Button as Button
-import Bootstrap.ListGroup as Listgroup
-import Bootstrap.Modal as Modal
 import Bootstrap.Form.Input as Input
-import Bootstrap.Button as Button
 
-import Shared exposing (..)
-
+import Api exposing (Cred, Service, register)
 
 
-userView : Model -> Html Msg
-userView model =
-    let 
-        content =
-            case model.maybeCred of
-                Nothing -> 
-                    [  welcoming, div [ class "p-4" ] [ form model ] ]
 
-                Just cred -> 
-                    []
-    in
-        section [] content
+-- MODEL
+
+
+type alias Model =
+    { kratia : Service
+    , credentials : Maybe Cred
+    , nicknameInput : String
+    , loading : Bool
+    }
+
+
+init : Service -> Maybe Cred -> ( Model, Cmd Msg )
+init kratia credentials = 
+    (
+        { kratia = kratia
+        , credentials = credentials
+        , nicknameInput = "" 
+        , loading = False
+        }
+        , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg 
+    = Submitted
+    | EnteredNickname String
+    | Registered (Result Http.Error Cred)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of 
+        Submitted ->
+            ( { model | loading = True }, register 
+                { service = model.kratia
+                , nickname = model.nicknameInput 
+                , onResponse = Registered 
+                }
+            )
+
+        EnteredNickname nicknameInput ->
+            ( { model | nicknameInput = nicknameInput }, Cmd.none )
+
+        Registered (Ok cred) -> 
+            ( { model | credentials = Just cred, loading = False } , Cmd.none )
+
+        Registered (Err err) -> 
+            ( { model | loading = False }, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    section [] (case model.credentials of
+        Nothing -> 
+            [  welcoming, div [ class "p-4" ] [ form model ] ]
+
+        Just cred -> 
+            []
+    )
 
 
 welcoming : Html Msg
@@ -44,14 +92,14 @@ welcoming =
         [ h1 [] [ text "" ]
         , p [] [ text "Welcome to the Kratia Demo" ]
         , p [] [ text "Kratia empowers communities by enabling them with digital governance. It helps the communities grow, evolve and adapt by offering lego blocks for them to design their collaborative decision-making process." ]
-        , p [] [ text "To start with the demo, please register with a nickname:" ]
+        , p [] [ text "To start with the demo, please register with a nicknameInput:" ]
         ]
 
 
 form : Model -> Html Msg
 form model =
     Form.formInline
-        [ onSubmit RegisterMember ]
+        [ onSubmit Submitted ]
         [ Input.text [ Input.attrs
             [ placeholder "Nickname"
             , disabled model.loading
@@ -64,4 +112,3 @@ form model =
             ]
             [ text "Register" ]
         ]
-
