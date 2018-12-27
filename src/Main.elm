8e47a98819ced94435097e21fe3aaa81d79773e1
Francisco Aramburo <francoara@pm.me>
2018-12-27 19:15:15 +0100

    Model cleanup with registration redirect

--- src/Main.elm
+++ src/Main.elm
@@ -11,10 +11,10 @@ import Html.Attributes exposing (..)
 import Bootstrap.Navbar as Navbar
 import Bootstrap.Grid.Col as Col
 
-import Api exposing (Cred, username, Service(..))
-import Config exposing (Config, Flags, fromFlags)
+import Api exposing (Cred, Flags, Session, guest, username, withNavState)
 import Route exposing (Route)
-import Page as Page exposing (Page(..))
+import Page as Page
+import Page.Blank as Blk
 import Page.Registration as Reg
 
 
@@ -35,31 +35,23 @@ main =
 -- MODEL
 
 
-type alias Model =
-    { credentials : Maybe Cred
-    , config : Config
-    , navKey : Nav.Key
-    , navState : Navbar.State
-    , page : Page
-    }
+type Model 
+    = Redirect Session
+    | NotFound Session
+    | About Session
+    | Registration Reg.Model
+    | Ballots Session
 
 
 init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
     let 
-        config = fromFlags flags
-
         ( navState, navCmd ) =
             Navbar.initialState NavMsg
         
         ( model, routeCmd ) =
             changeRouteTo (Route.fromUrl url)
-                { credentials = Nothing
-                , config = config
-                , navKey = key
-                , navState = navState
-                , page = About
-                }
+                (Redirect (guest flags key navState))
     in
         ( model, Cmd.batch [ routeCmd, navCmd ] )
 
@@ -70,7 +62,11 @@ init flags url key =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Navbar.subscriptions model.navState NavMsg
+    let 
+        session =
+            lastSession model
+    in
+    Navbar.subscriptions session.navState NavMsg
 
 
 
@@ -86,9 +82,72 @@ type Msg
     | RegMsg Reg.Msg
 
 
+lastSession : Model -> Session
+lastSession model = 
+    case model of 
+        Redirect session ->
+            session
+        
+        NotFound session ->
+            session
+        
+        About session -> 
+            session
+        
+        Registration registration ->
+            Reg.toSession registration
+
+        Ballots session ->
+            session
+
+
+updateSession : (Session -> Session) -> Model -> Model
+updateSession updt model = 
+    case model of 
+        Redirect session ->
+            Redirect <| updt session
+        
+        NotFound session ->
+            NotFound <| updt session
+        
+        About session -> 
+            About <| updt session
+        
+        Registration registration ->
+            Registration <| Reg.updateSession updt registration
+
+        Ballots session ->
+            Ballots <| updt session
+
+
+changeRouteTo : Maybe Route -> Model -> ( Model, Cmd Msg )
+changeRouteTo maybeRoute model =
+    let 
+        session =
+            lastSession model
+    in
+    case maybeRoute of
+        Nothing ->
+            ( NotFound session, Cmd.none )
+        
+        Just Route.About ->
+            ( About session, Cmd.none )
+
+        Just Route.Registration ->
+            Reg.init session  
+                |> updateWith Registration RegMsg model
+
+        Just Route.Ballots ->
+            ( Ballots session, Cmd.none )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case ( msg, model.page ) of
+    let 
+        session =
+            lastSession model
+    in
+    case ( msg, model ) of
         ( RouteChanged route, _ ) ->
             changeRouteTo route model
 
@@ -96,12 +155,12 @@ update msg model =
             changeRouteTo (Route.fromUrl url) model
 
         ( LinkClicked req, _ ) ->
-             case req of
-                 Browser.Internal url -> ( model, Nav.pushUrl model.navKey <| Url.toString url )
-                 Browser.External href -> ( model, Nav.load href )
+            case req of
+                Browser.Internal url -> ( model, Nav.pushUrl session.navKey <| Url.toString url )
+                Browser.External href -> ( model, Nav.load href )
 
         ( NavMsg state, _ ) -> 
-            ( { model | navState = state } , Cmd.none )
+            ( model |> updateSession (withNavState state), Cmd.none )
 
         ( RegMsg subMsg, Registration registration ) -> 
             Reg.update subMsg registration
@@ -112,26 +171,9 @@ update msg model =
             ( model, Cmd.none )
 
 
-changeRouteTo : Maybe Route -> Model -> ( Model, Cmd Msg )
-changeRouteTo maybeRoute model =
-    case maybeRoute of
-        Nothing ->
-            ( { model | page = NotFound }, Cmd.none )
-        
-        Just Route.About ->
-            ( { model | page = About }, Cmd.none )
-
-        Just Route.Registration ->
-            Reg.init model.config.kratia model.credentials
-                |> updateWith Registration RegMsg model
-
-        Just Route.Ballots ->
-            ( { model | page = Ballots }, Cmd.none )
-
-
-updateWith : (subModel -> Page) -> (subMsg -> Msg) -> Model -> ( subModel, Cmd subMsg ) -> ( Model, Cmd Msg )
+updateWith : (subModel -> Model) -> (subMsg -> Msg) -> Model -> ( subModel, Cmd subMsg ) -> ( Model, Cmd Msg )
 updateWith toModel toMsg model ( subModel, subCmd ) =
-    ( { model | page = toModel subModel }, Cmd.map toMsg subCmd )
+    ( toModel subModel, Cmd.map toMsg subCmd )
 
 
 
@@ -142,12 +184,13 @@ view : Model -> Browser.Document Msg
 view model =
     let
         session =
-            { credentials = model.credentials
-            , state = model.navState
-            }
+            lastSession model
     in
-    case model.page of
-        NotFound ->
+    case model of
+        Redirect _ ->
+            Page.view session (\_ -> Ignored) NavMsg Blk.view
+
+        NotFound _ ->
             Page.view session (\_ -> Ignored) NavMsg
                 { title = "404"
                 , content =
@@ -163,13 +206,13 @@ view model =
                 , content = Reg.view registration
                 }
 
-        Ballots ->
+        Ballots _ ->
             Page.view session (\_ -> Ignored) NavMsg
-                { title = "404"
+                { title = "Ballots"
                 , content = h1 [] [ text "Ballots" ] 
                 }
 
-        About ->
+        About _ ->
             Page.view session (\_ -> Ignored) NavMsg
                 { title = "About"
                 , content = h1 [] [ text "About" ]
