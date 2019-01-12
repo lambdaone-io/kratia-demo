96dc5f62d3f0711ab3784cb01b10e0bb140f4111
Francisco Aramburo <francoara@pm.me>
2019-01-12 15:47:00 +0100

    rendering github ballots

--- src/Kratia/Ballot.elm
+++ src/Kratia/Ballot.elm
@@ -4,6 +4,7 @@ module Kratia.Ballot exposing (Ballot, ClosedBallot, decoder, decoderClosed)
 import Time exposing (Posix, millisToPosix)
 import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Pipeline exposing (required)
+import GitHub.PullRequest exposing (PullRequestEvent, eventDecoder)
 
 
 
@@ -14,14 +15,14 @@ type alias Ballot =
     { ballotBox : String
     , ballot : List String
     , closesOn : Posix
-    , data : String
+    , data : PullRequestEvent
     }
 
 
 type alias ClosedBallot =
     { ballotBox : String
     , closedOn : Posix
-    , data : String
+    , data : PullRequestEvent
     , resolution : List String
     }
 
@@ -32,7 +33,7 @@ decoder =
         |> required "ballotBox" Decode.string
         |> required "ballot" ( Decode.list ( Decode.string ) )
         |> required "closesOn" decodePosix
-        |> required "data" Decode.string
+        |> required "data" eventDecoder
 
 
 decoderClosed : Decoder ClosedBallot
@@ -40,7 +41,7 @@ decoderClosed =
     Decode.succeed ClosedBallot
         |> required "address" Decode.string
         |> required "closedOn" decodePosix
-        |> required "data" Decode.string
+        |> required "data" eventDecoder
         |> required "resolution" ( Decode.list ( Decode.string ) )
 
 
